bd7b909391cd55466929243d22bdad070a69b84f
astellon <22632787+astellon@users.noreply.github.com>
2020-04-17 11:27:34 +0900

    Multiprocessing

--- makeFileList.py
+++ makeFileList.py
@@ -5,59 +5,68 @@ import pdb, os, glob, argparse, cv2
 from scipy.io import wavfile
 from tqdm import tqdm
 
-parser = argparse.ArgumentParser(description = "TrainArgs");
+from multiprocessing import Pool
 
-parser.add_argument('--mp4_dir', type=str, default="voxceleb2/dev/mp4", help='');
-parser.add_argument('--txt_dir', type=str, default="voxceleb2/dev/txt", help='');
-parser.add_argument('--wav_dir', type=str, default="voxceleb2/dev/wav", help='');
-parser.add_argument('--output',  type=str, default="data/dev.txt", help='');
+parser = argparse.ArgumentParser(description = "TrainArgs")
 
-args = parser.parse_args();
+parser.add_argument('--mp4_dir', type=str, default="voxceleb2/dev/mp4", help='')
+parser.add_argument('--txt_dir', type=str, default="voxceleb2/dev/txt", help='')
+parser.add_argument('--wav_dir', type=str, default="voxceleb2/dev/wav", help='')
+parser.add_argument('--output',  type=str, default="data/dev.txt", help='')
+
+args = parser.parse_args()
 
 files = glob.glob(args.mp4_dir+'/*/*/*.mp4')
 
 g = open(args.output,'w')
 
-for fname in tqdm(files):
+def get(fname):
+  wavname = fname.replace(args.mp4_dir,args.wav_dir).replace('.mp4','.wav')
+  txtname = fname.replace(args.mp4_dir,args.txt_dir).replace('.mp4','.txt')
 
-	wavname = fname.replace(args.mp4_dir,args.wav_dir).replace('.mp4','.wav')
-	txtname = fname.replace(args.mp4_dir,args.txt_dir).replace('.mp4','.txt')
+  ## Read offset
+  f = open(txtname,'r')
+  txt = f.readlines()
+  f.close()
 
-	## Read offset
-	f = open(txtname,'r')
-	txt = f.readlines()
-	f.close()
+  if txt[2].split()[0] == 'Offset':
+    offset = txt[2].split()[2]
+  else:
+    print('Skipped %s - unable to read offset'%fname)
+    return None
 
-	if txt[2].split()[0] == 'Offset':
-		offset = txt[2].split()[2]
-	else:
-		print('Skipped %s - unable to read offset'%fname)
-		continue;
+  ## Read video length
+  cap = cv2.VideoCapture(fname)
+  counted_frames = 0
+  while True:
+    ret, image = cap.read()
+    if ret == 0:
+      break
+    else:
+      counted_frames += 1
+  total_frames = cap.get(7)
+  cap.release()
 
-	## Read video length
-	cap = cv2.VideoCapture(fname)
-	counted_frames = 0
-	while True:
-		ret, image = cap.read()
-		if ret == 0:
-			break
-		else:
-			counted_frames += 1
-	total_frames = cap.get(7)
-	cap.release()
+  if total_frames != counted_frames:
+    print('Skipped %s - frame number inconsistent'%fname)
+    return None
 
-	if total_frames != counted_frames:
-		print('Skipped %s - frame number inconsistent'%fname)
-		continue;
+  ## Read audio
+  sample_rate, audio  = wavfile.read(wavname)
 
-	## Read audio
-	sample_rate, audio  = wavfile.read(wavname)
+  lendiff = len(audio)/640 - counted_frames
 
-	lendiff = len(audio)/640 - counted_frames
+  if abs(lendiff) > 1:
+    print('Skipped %s - audio and video lengths different'%fname)
+    return None
 
-	if abs(lendiff) > 1:
-		print('Skipped %s - audio and video lengths different'%fname)
-		continue;
+  return fname, wavname, offset, counted_frames
 
-	g.write('%s %s %s %d\n'%(fname,wavname,offset,counted_frames))
-	
+with Pool(56) as pool:
+  ret = pool.imap(get, files)
+  result = list(tqdm(ret, total=len(files)))
+
+for r in result:
+  if r is not None:
+    fname, wavname, offset, counted_frames = r
+    g.write('%s %s %s %d\n'%(fname,wavname,offset,counted_frames))
